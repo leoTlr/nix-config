485f4767d0d2c858f9a9d448ac811a678e97d9f2
leoTlr <ltlr+github@posteo.de>
2024-02-13 00:10:33 +0100

    add home-manager

--- flake.nix
+++ flake.nix
@@ -3,23 +3,28 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   
-  outputs = {self, nixpkgs, ... }:
+  outputs = {self, nixpkgs, home-manager, ... }:
     let
-      lib = nixpkgs.lib; 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       testbox = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ./configuration.nix
-          # inputs.home-manager.nixosModules.default 
-        ];
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      leo = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
