34e7513337daba9e7abc4da2093231dc85fd5941
leoTlr <ltlr+github@posteo.de>
2024-02-16 20:46:25 +0100

    bootstrap from flake

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 device="${1:?device not set}"
 lukspw="${2:?lukspw not set}"
+flake_uri="${3:-github:leoTlr/nix-config#testbox}"
 
 echo "### Partitioning ${device}"
 ${SCRIPT_DIR}/partition.sh ${device} ${lukspw}
 echo "### Partitioning ${device} successful"
 
-echo "### Installing nixos"
-mkdir -p /mnt/etc/nixos/
-cp ${SCRIPT_DIR}/hosts/testbox/configuration.nix /mnt/etc/nixos/configuration.nix
-
-nixos-generate-config --root /mnt
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-
-nixos-install
+echo "### Installing nixos from flake ${flake_uri}"
+nix-shell --packages git --run "nixos-install --flake ${flake_uri}"
 echo "### Successfully installed nixos. Reboot now"
