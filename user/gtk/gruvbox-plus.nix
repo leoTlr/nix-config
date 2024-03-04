4fbde32b85bd527642ede07423dc022ef8dbf6df
leoTlr <ltlr+github@posteo.de>
2024-03-04 20:24:56 +0100

    gtk: bump icon pack version

--- user/gtk/gruvbox-plus.nix
+++ user/gtk/gruvbox-plus.nix
@@ -1,12 +1,14 @@
-{pkgs}: let
-  link = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v3.1/gruvbox-plus-icon-pack-3.1.zip";
+{ pkgs }: 
+let
+  # nix hash to-sri sha256:$(nix-prefetch-url https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v5.1/gruvbox-plus-icon-pack-5.1.zip --type sha256)
+  link = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v5.1/gruvbox-plus-icon-pack-5.1.zip";
 in
   pkgs.stdenv.mkDerivation {
     name = "gruvbox-plus";
 
     src = pkgs.fetchurl {
       url = link;
-      sha256 = "sha256-i/AzhYz/ACeXsG5j0kDVfvfA4TwxA3KZJTPwCO4BKmc=";
+      sha256 = "sha256-QB6QHBKUMXtXN6DYCCvMWJZtcFz4Al/XRlLhGibHcNg=";
     };
 
     dontUnpack = true;
