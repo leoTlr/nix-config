0b07471a603b31b3906205bc504a8dea21ca6139
leoTlr <ltlr+github@posteo.de>
2024-03-18 22:27:24 +0100

    refactor: hyprland: make option

--- system/hyprland/hyprland.nix
+++ system/hyprland/hyprland.nix
@@ -1,33 +1,46 @@
-{ pkgs, config, commonSettings, ... }:
+{ pkgs, config, lib, commonSettings, ... }:
 
-{
+{ 
+  options.hyprland = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use hyprland window manager";
+    };
+  };
+
   imports = [
     ./pipewire.nix
     ./wayland.nix
     ./dbus.nix
     ../greetd.nix
   ];
-
-  environment.systemPackages = with pkgs; [
-    polkit
-    xdg-desktop-portal-hyprland
-    dconf
-  ];
   
-  programs.hyprland = {
-    enable = true;
-    xwayland = {
-      enable = true;
-    };
-    portalPackage = pkgs.xdg-desktop-portal-hyprland;
-  };
+  config = lib.mkIf config.hyprland.enable {
 
-  greetd = {
-    enable = true;
-    command = if config.isVmGuest then
-    ''sh -c "WLR_RENDERER_ALLOW_SOFTWARE=1 ${pkgs.hyprland}/bin/Hyprland"''
-    else
-    "${pkgs.hyprland}/bin/Hyprland";
+    environment.systemPackages = with pkgs; [
+      polkit
+      xdg-desktop-portal-hyprland
+      dconf
+    ];
+  
+    programs.hyprland = {
+      enable = true;
+      xwayland = {
+        enable = true;
+      };
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    };
+
+    greetd = {
+      enable = true;
+      command = if config.isVmGuest then
+      ''sh -c "WLR_RENDERER_ALLOW_SOFTWARE=1 ${pkgs.hyprland}/bin/Hyprland"''
+      else
+      "${pkgs.hyprland}/bin/Hyprland";
+    };
+    
   };
+  
 
 }
