c4898fac3de09f73fb69715575d8fc9ab1ce3ee1
leoTlr <ltlr+github@posteo.de>
2024-04-11 21:27:44 +0200

    add user pw

--- profiles/base/system/default.nix
+++ profiles/base/system/default.nix
@@ -1,10 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 let
   cfg = config.profiles.base;
 in
 {
   imports = [
     ../../../system
+    inputs.sops-nix.nixosModules.default
   ];
 
   options.profiles.base = import ./interface.nix { inherit lib; };
--- profiles/base/system/settings.nix
+++ profiles/base/system/settings.nix
@@ -3,6 +3,7 @@ let
   cfg = config.profiles.base;
 in
 {
+
   system.stateVersion = cfg.system.stateVersion;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -16,10 +17,22 @@ in
     networkmanager.enable = true;
   };
 
+  # https://github.com/Mic92/sops-nix?tab=readme-ov-file#setting-a-users-password
+  sops = {
+    defaultSopsFile = ../../../secrets.yaml;
+    defaultSopsFormat = "yaml";
+    gnupg.home = "/home/${cfg.system.mainUser.name}/.gnupg";
+    secrets."${cfg.system.mainUser.name}/passwordHash".neededForUsers = true;
+  };
+
   syslib = {
-    users.mainUser = {
-      inherit (cfg.system.mainUser) name;
-      shell = pkgs.fish;
+    users = {
+      mutable = false;
+      mainUser = {
+        inherit (cfg.system.mainUser) name;
+        shell = pkgs.fish;
+        passwordHashPath = config.sops.secrets."${cfg.system.mainUser.name}/passwordHash".path;
+      };
     };
 
     localization.enable = true;
