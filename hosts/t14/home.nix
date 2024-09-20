5cfbadff83e70e74b028b00e2d166bb61fcc94d7
leoTlr <ltlr@posteo.de>
2024-09-20 19:05:59 +0200

    fix: git sign email
    For github to recognize the commit as a verified commit the email of the
    signature must match the committer email. Otherwise it shows as
    "unverified" even though the signing pubkey is present in github
    settings.
    
    Also it makes more sense to not use my sub-mail for commits because the
    commit is bound to my identity and not my github email.

--- hosts/t14/home.nix
+++ hosts/t14/home.nix
@@ -18,7 +18,7 @@ in
     system.configName = sysConfig;
     gitInfo = {
       name = "leoTlr";
-      email = "ltlr+github@posteo.de";
+      email = "ltlr@posteo.de";
       signKey = "17F0A6278F9E22B4A846DAEAE0CF76180D567EDF";
   };
 
