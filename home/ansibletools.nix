5c98969ccdb5fcce3b52ca6f1e186f1bd7581fd5
leoTlr <ltlr+github@posteo.de>
2025-03-24 10:20:55 +0100

    fix: ansible: invhosts script: dont show ansible-inventory stderr

--- home/ansibletools.nix
+++ home/ansibletools.nix
@@ -10,7 +10,7 @@ let
     text = ''
       inv_dir=${ansibleInventoryDir}
       inv_choice=$(find "$inv_dir" -name "*.yml" -maxdepth 1 | fzf --height="12%" --layout=reverse)
-      groups_json=$(ansible-inventory --export --list -i "$inv_choice" \
+      groups_json=$(ansible-inventory --export --list -i "$inv_choice" 2>/dev/null \
         | jq '. | to_entries | map_values(select(.value.hosts != null)) | map_values( {"key": .key, "value": .value.hosts}) | from_entries')
       group_names=$(echo "$groups_json" | jq -r '. | keys[]')
       echo "$group_names" | fzf --height="12%" --layout=reverse | jq -rR --argjson groups "$groups_json" '$groups.[.] | join("\n")'
