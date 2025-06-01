effdf7294fedf619110412780cfe7bd5a36c2551
leoTlr <ltlr@posteo.de>
2025-06-01 18:50:55 +0200

    feat: trust homelab ca

--- hosts/liveiso/configuration.nix
+++ hosts/liveiso/configuration.nix
@@ -9,6 +9,23 @@
 
   users.defaultUserShell = pkgs.fish;
 
+  security.pki.certificates = [
+    ''
+      homelab ca
+      =========
+      -----BEGIN CERTIFICATE-----
+      MIIBcDCCARagAwIBAgIRAN3CHo6NnQP3WKHPi54y2GkwCgYIKoZIzj0EAwIwFjEU
+      MBIGA1UEAxMLTGVvIFJvb3QgQ0EwHhcNMjUwNTMxMjAyNTA3WhcNMzUwNTI5MjAy
+      NTA3WjAWMRQwEgYDVQQDEwtMZW8gUm9vdCBDQTBZMBMGByqGSM49AgEGCCqGSM49
+      AwEHA0IABDL2Q7xJlPFeBQEWdjPW+MSF5TpROKm6iUr9aZncL87HKOf0cvLVlS8f
+      b3xu2fH9Ulydi6Svo4+W5PYLZ984yuOjRTBDMA4GA1UdDwEB/wQEAwIBBjASBgNV
+      HRMBAf8ECDAGAQH/AgEBMB0GA1UdDgQWBBQ8dlUBEU+WrEUCAytSSezZwQxw0TAK
+      BggqhkjOPQQDAgNIADBFAiAbJ0avET36RAWjQMjKEopx32UjNV/tORzWfF1vZ5h3
+      KgIhAKO2RshAHEuQasocZeqmPZJG263Nb6ApUBx5QwobrXlq
+      -----END CERTIFICATE-----
+    ''
+  ];
+
   syslib = {
 
     nix = {
