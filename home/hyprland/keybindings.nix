34e083cfb8791c51800230ec2aaec638ca5eef1a
leoTlr <ltlr@posteo.de>
2025-10-08 22:51:06 +0200

    fix: hyprland: window resize key
    by defining a combination with Alt, the key gets consumed by hyprland and not sent to the window.
    For firefox this breaks the Alt+leftarrow shortcut to go back.
    
    Using binr works but then triggers both a window resize and the shortcut
    -> better use a diferent key

--- home/hyprland/keybindings.nix
+++ home/hyprland/keybindings.nix
@@ -90,11 +90,11 @@ in
     ",XF86MonBrightnessDown,    exec, ${brightness_down}"
     ",XF86MonBrightnessUp,      exec,  ${brightness_up}"
 
-    # resize windows with alt + arrow keys
-    "ALT,right,        resizeactive, 15    0"
-    "ALT,left,         resizeactive, -15   0"
-    "ALT,up,           resizeactive, 0   -15"
-    "ALT,down,         resizeactive, 0    15"
+    # resize windows with arrow keys
+    "CTRL, right,        resizeactive, 15    0"
+    "CTRL, left,         resizeactive, -15   0"
+    "CTRL, up,           resizeactive, 0   -15"
+    "CTRL, down,         resizeactive, 0    15"
   ];
 
   bindl = [
