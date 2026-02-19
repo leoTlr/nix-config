37df0fad564175a944892748fb7fe4b53f8f41bb
leoTlr <ltlr@posteo.de>
2026-02-20 00:01:30 +0100

    cfg: t15: font sizes + display scaling
    display scaling makes issues for games. They seem to cant use the full display resoultion

--- hosts/t15/home.nix
+++ hosts/t15/home.nix
@@ -10,7 +10,7 @@ _:
     desktop = {
       enable = true;
       monitors = [
-        "eDP-1,2560x1600@240,0x0,1.25,vrr,1"
+        "eDP-1,2560x1600@240,0x0,1,vrr,1"
         "DP-1,1920x1080@60,1920x0,1,vrr,1"
         "DP-2,1920x1080@60,3840x0,1,vrr,1"
       ];
@@ -24,6 +24,9 @@ _:
     };
   };
 
-  homelib.hyprland.keyMap = "us";
+  homelib = {
+    hyprland.keyMap = "us";
+    stylix.fontSize = 14;
+  };
 
 }
