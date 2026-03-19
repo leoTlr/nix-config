75867d1a7f861b5e15ac6eba1d6c871aac1c4a72
leoTlr <ltlr+github@posteo.de>
2026-03-19 12:58:24 +0100

    ft: aliases

--- home/fish.nix
+++ home/fish.nix
@@ -18,24 +18,36 @@ in
   options.homelib.fish.enable = lib.mkEnableOption "fish shell";
 
   config = lib.mkIf cfg.enable {
+
     programs.fish = {
       enable = true;
+
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
       '';
+
       shellAliases = {
         ".." = "cd ..";
         "..." = "cd ../..";
-        g = "git";
-        ls = "eza";
-        la = "eza -lah";
-        rgf = "rg --files";
+        g = lib.getExe pkgs.git;
+        ls = lib.getExe pkgs.eza;
+        la = lib.getExe pkgs.eza + " -lah";
+        rgf = lib.getExe pkgs.ripgrep + " --files";
+        rg = lib.getExe pkgs.ripgrep + " --ignore-case";
+        tldr = "tldr --platform linux";
+        tldrm = "tldr --platform macos";
+        y = lib.getExe pkgs.yazi;
       } // lib.optionalAttrs pkgs.stdenv.isLinux linuxSettings.shellAliases;
+
       shellInit = ''
         function digs; dig +short $argv[1] | uniq | head -n1; end
         function mkcd; mkdir $argv[1] && cd $argv[1]; end
         function icat; kitten icat $argv[1]; end #show images in kitty terminal
-        function nsp; nix shell nixpkgs#$argv; end
+        function nsp
+          set --local --export NIXPKGS_ALLOW_UNFREE 1
+          set --local --export NIXPKGS_ALLOW_INSECURE 1
+          nix shell --impure nixpkgs#$argv
+        end
       '';
     };
 
