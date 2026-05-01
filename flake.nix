7eddfde47372da801eae8ebbefe034fed90b37e7
leoTlr <ltlr@posteo.de>
2026-05-01 11:18:44 +0200

    ft: bump nixpkgs release

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Personal config preferences";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-23-05.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager-unstable = {
